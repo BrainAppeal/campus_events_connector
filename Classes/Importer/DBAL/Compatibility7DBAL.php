69e5b8b8477c2408117ccdf10b2d9b5e34fec0b8
Joshua Billert <Joshua.Billert@Brain-Appeal.com>
2018-08-31 10:01:50 +0200

    Compatibility fix for file references and typo3 v7

--- Classes/Importer/DBAL/Compatibility7DBAL.php
+++ Classes/Importer/DBAL/Compatibility7DBAL.php
@@ -65,4 +65,58 @@ class Compatibility7DBAL extends \BrainAppeal\BrainEventConnector\Importer\DBAL\
         }
     }
 
+    private $sysFileReferenceWorkaroundInitialized = false;
+    private $backupIgnoreRootLevelRestriction;
+
+    private function enableSysFileReferenceWorkaround()
+    {
+        if (!$this->sysFileReferenceWorkaroundInitialized) {
+            $this->backupIgnoreRootLevelRestriction = $GLOBALS['TCA']['sys_file_reference']['ctrl']['security']['ignoreRootLevelRestriction'];
+            $this->sysFileReferenceWorkaroundInitialized = true;
+        }
+
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['tx_braineventconnector_compatibility7'] = \BrainAppeal\BrainEventConnector\Compatibility\DatabaseAccessHook::class;
+        $GLOBALS['TCA']['sys_file_reference']['ctrl']['security']['ignoreRootLevelRestriction'] = true;
+    }
+
+    private function disableSysFileReferenceWorkaround()
+    {
+        if ($this->sysFileReferenceWorkaroundInitialized) {
+            $GLOBALS['TCA']['sys_file_reference']['ctrl']['security']['ignoreRootLevelRestriction'] = $this->backupIgnoreRootLevelRestriction;
+        }
+        if (array_key_exists('tx_braineventconnector_compatibility7', $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'])) {
+            unset($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['tx_braineventconnector_compatibility7']);
+        }
+    }
+
+    /**
+     * @param \TYPO3\CMS\Core\Resource\File $sysFile
+     * @param \BrainAppeal\BrainEventConnector\Domain\Model\ImportedModelInterface $target
+     * @param string $field
+     * @param array $attribs
+     */
+    public function addSysFileReference($sysFile, $target, $field, $attribs = [])
+    {
+        $pidBackup = $target->getPid();
+        $target->setPid(0);
+
+        $this->enableSysFileReferenceWorkaround();
+        parent::addSysFileReference($sysFile, $target, $field, $attribs);
+        $this->disableSysFileReferenceWorkaround();
+
+        $target->setPid($pidBackup);
+    }
+
+    /**
+     * @param \TYPO3\CMS\Extbase\Domain\Model\FileReference $fileReference
+     * @param array $attribs
+     */
+    public function updateSysFileReference($fileReference, $attribs = [])
+    {
+        $this->enableSysFileReferenceWorkaround();
+        parent::updateSysFileReference($fileReference, $attribs);
+        $this->disableSysFileReferenceWorkaround();
+    }
+
+
 }
