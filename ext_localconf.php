ec22ed5805250c17ef49db291fd6a8ea49c328cc
BrainAppeal <info@brain-appeal.com>
2020-10-09 11:00:00 +0200

    Release 1.0.6 https://www.campus-events.com/

--- ext_localconf.php
+++ ext_localconf.php
@@ -13,21 +13,27 @@
 
 defined('TYPO3_MODE') or die();
 
-// Add caching framework garbage collection task
-/** @var string $_EXTKEY */
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\BrainAppeal\CampusEventsConnector\Task\EventImportTask::class] = array(
-        'extension' => $_EXTKEY,
-        'title' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang.xlf:tx_campuseventsconnector_task_eventimporttask.name',
-        'description' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang.xlf:tx_campuseventsconnector_task_eventimporttask.description',
-        'additionalFields' => \BrainAppeal\CampusEventsConnector\Task\EventImportAdditionalFieldProvider::class
-);
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['importFieldNamesUpdateWizard'] = \BrainAppeal\CampusEventsConnector\Updates\ImportFieldNamesUpdateWizard::class;
-if (TYPO3_MODE === 'BE') {
-    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-    $iconRegistry->registerIcon(
-        'ext-convertconfiguration-type-default',
-        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        ['source' => 'EXT:'.$_EXTKEY.'/Resources/Public/Icons/tx_campuseventsconnector_domain_model_convertconfiguration.svg']
-    );
-}
+call_user_func(
+    function($extKey) {
+        // Add caching framework garbage collection task
+        /** @var string $extKey */
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\BrainAppeal\CampusEventsConnector\Task\EventImportTask::class] = array(
+                'extension' => $extKey,
+                'title' => 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang.xlf:tx_campuseventsconnector_task_eventimporttask.name',
+                'description' => 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang.xlf:tx_campuseventsconnector_task_eventimporttask.description',
+                'additionalFields' => \BrainAppeal\CampusEventsConnector\Task\EventImportAdditionalFieldProvider::class
+        );
+
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['importFieldNamesUpdateWizard'] = \BrainAppeal\CampusEventsConnector\Updates\ImportFieldNamesUpdateWizard::class;
+        if (TYPO3_MODE === 'BE') {
+            $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+            $iconRegistry->registerIcon(
+                'ext-convertconfiguration-type-default',
+                \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+                ['source' => 'EXT:'.$extKey.'/Resources/Public/Icons/tx_campuseventsconnector_domain_model_convertconfiguration.svg']
+            );
+        }
+    },
+    'campus_events_connector'
+);
